c2fb78f1c3232b48e34a782aabcc67ec9cfbd849
webkatu <mail@webkatu.com>
2017-04-24 22:43:12 +0900

    disabled fetch cache

--- client/config.js
+++ client/config.js
@@ -2,5 +2,6 @@ export default {
 	apiServer: 'http://192.168.1.138:3001',
 	defaultHeaders: {
 		'X-Requested-With': 'XMLHttpRequest',
+		'If-Modified-Since': 'Thu, 01 Jun 1970 00:00:00 GMT',
 	}
 }
